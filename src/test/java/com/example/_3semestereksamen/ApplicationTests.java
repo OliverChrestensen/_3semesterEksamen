6b2557474ea427e1a8de6537c2e237fe478cf97b
OliverChrestensen <oliver.chrestensen@hotmail.com>
2021-12-20 09:09:08 +0100

    Initial commit

---
+++ src/test/java/com/example/_3semestereksamen/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example._3semestereksamen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
